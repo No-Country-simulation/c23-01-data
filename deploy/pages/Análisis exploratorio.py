82be7481f14da99e6ddb66c61b77ded15b616a0c
Juan Guerrero <juan.guerrero@multiversecomputing.com>
2025-02-06 12:53:04 -0400

    Algunos cambios. Deploy basico

---
+++ deploy/pages/Análisis exploratorio.py
@@ -0,0 +1,162 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+
+st.set_page_config(page_title='Nombre de nuestra app', page_icon="blue_heart", layout='wide') #page_icon: puede ser un emoticon, una imagen..
+#st.markdown("<h1 style='text-align: center; color: red;'>Some title</h1>", unsafe_allow_html=True)
+
+df = pd.read_csv('Data/diabetes_renamed.csv')
+df_cat = pd.read_csv('Data/diabetes_cat.csv')
+
+### Define dictionaries: they will be useful later
+
+binary_dict = {0:'No',1:'Yes'} ### Dictionary for binary variables
+diagnose_dict = {0:'Non-diabetic',1:'Diabetic'} ### Dictionary for the diabetes_status
+sex_dict = {0:'Female',1:'Male' }### Sex Dictionary
+general_health_dict = {1:'Excellent',2:'Very good',3:'Good',4:'Fair',5:'Poor'} ### General Health Dictionary
+age_dict = {1:'18-24', 2:'25-29', 3:'30-34', 4:'35-39', 5:'40-44', 6:'45-49', 7:'50-54', 8:'55-59', 9:'60-64', 10:'65-69', 11:'70-74', 12:'75-79', 13:'above 80'}
+education_dict = {1:"Kindergarten",2:"Elementary",3:"Some high school",4:"High school graduate",5:"Some college/tecnical school",6:"College graduate"} ### Education
+income_dict = {1:"0-10K",2:"10-15K",3:"15-20K",4:"20-25K",5:"25-30K",6:"30-35K",7:"35-50K",8:"50-75K"}
+
+
+
+def main():
+    st.markdown("<h1 style='text-align: center'> Nombre de nuestra app </h1>", unsafe_allow_html=True)
+
+    st.markdown("## Datos: encuestas de la CDC del año 2015")
+
+
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.markdown(f"<h3 style='text-align: center'> Total de encuestados: {len(df)} </h3>", unsafe_allow_html=True)
+
+        #st.markdown("<h3 style='text-align: center'> Percent count </h3>", unsafe_allow_html=True)
+        fig1 = px.pie(df_cat,names="Diabetes_status",color='Diabetes_status',color_discrete_map={'Non-diabetic': 'green', 'Diabetic': 'red'}, width=350, height=350)
+            # Update layout for better aesthetics
+        fig1.update_traces(texttemplate='%{percent:.1%}', textposition='inside')
+        fig1.update_layout(
+        yaxis_title="Diabetes status", 
+        xaxis_title="Frequency [Count]",
+        legend_title_text='Diabetes Status',
+        template='plotly_white',  # Similar to seaborn's "whitegrid" style
+         margin=dict(t=0, b=0, l=0, r=0),
+        font=dict(
+            size=20  # General font size for other text elements
+        ),
+        legend=dict(font=dict(size= 20)),
+        legend_title=dict(font=dict(size= 22))
+        )
+        st.plotly_chart(fig1,use_container_width=True)
+ 
+
+    #col3, col4 = st.columns(2)
+
+    with col2:
+        st.markdown("### Distribución por sexo")
+        fig2 = px.histogram(
+            df_cat,
+            y='Diabetes_status',
+            color='Sex',
+            barmode='group',
+            color_discrete_map={'Female': 'hotpink', 'Male': 'blue'},
+        )
+        fig2.update_layout(
+            xaxis_title="Frequency [Count]",
+            yaxis_title="Diabetes status",
+            xaxis=dict(tickfont=dict(size=16)),  # X-axis tick font size
+            yaxis=dict(tickfont=dict(size=16)),  # Y-axis tick font size
+            xaxis_title_font_size=20,  # X-axis title font size
+            yaxis_title_font_size=20,  # Y-axis title font size
+            legend=dict(font=dict(size= 20)),
+            legend_title=dict(font=dict(size= 22))
+        )
+        st.plotly_chart(fig2,use_container_width=True)
+
+    with col3:
+        st.markdown("### Distribución etaria")
+        fig3 = px.histogram(
+            df_cat,
+            color='Diabetes_status',
+            x='Age',
+            barmode='group',
+            color_discrete_map={'Non-diabetic': 'blue', 'Diabetic': 'red'},
+        )
+        fig3.update_layout(xaxis_title="Diabetes status",
+                           yaxis_title="Frequency [Count]", 
+                           showlegend = False,
+                            xaxis=dict(tickfont=dict(size=16)),  # X-axis tick font size
+                            yaxis=dict(tickfont=dict(size=16)),  # Y-axis tick font size
+                            xaxis_title_font_size=20,  # X-axis title font size
+                            yaxis_title_font_size=20,  # Y-axis title font size
+                           )
+        fig3.update_xaxes(categoryorder='array', categoryarray= list(age_dict.values()))
+        st.plotly_chart(fig3,use_container_width=True)     
+
+
+    cola, colb, colc = st.columns(3)
+
+    with cola:
+        st.markdown("## Distribución por Indice de masa corporal")   
+        figa = px.box(
+            df_cat,
+            y="BMI",
+            x="Diabetes_status",
+            color="Diabetes_status",
+            color_discrete_sequence=['blue', 'red'],
+            #box=True,      # Adds a box plot inside the violin
+            #points="all"   # Shows all individual points
+        ) 
+        figa.update_layout(xaxis_title="Diabetes status", 
+                           yaxis_title="BMI", 
+                           showlegend = False,
+                            xaxis=dict(tickfont=dict(size=16)),  # X-axis tick font size
+                            yaxis=dict(tickfont=dict(size=16)),  # Y-axis tick font size
+                            xaxis_title_font_size=20,  # X-axis title font size
+                            yaxis_title_font_size=20,  # Y-axis title font size
+                           )
+        st.plotly_chart(figa,use_container_width=True) 
+
+    with colb:
+        st.markdown("## Salud general")
+        figb = px.violin(
+            df_cat,
+            y="General_health",
+            x="Diabetes_status",
+            color="Diabetes_status",
+            color_discrete_sequence=['blue', 'red']
+        )
+        figb.update_yaxes(categoryorder='array', categoryarray= list(general_health_dict.values()))
+        figb.update_layout(xaxis_title="Diabetes status", 
+                           yaxis_title="General health", 
+                           showlegend = False,
+                            xaxis=dict(tickfont=dict(size=16)),  # X-axis tick font size
+                            yaxis=dict(tickfont=dict(size=16)),  # Y-axis tick font size
+                            xaxis_title_font_size=20,  # X-axis title font size
+                            yaxis_title_font_size=20,  # Y-axis title font size
+                           )
+        st.plotly_chart(figb,use_container_width=True)
+
+    with colc:
+        st.markdown("## Distribución por ingresos")
+        figc = px.violin(
+            df_cat,
+            y="Income",
+            x="Diabetes_status",
+            color="Diabetes_status",
+            color_discrete_sequence=['blue', 'red'],
+        )
+        figc.update_yaxes(categoryorder='array', categoryarray= list(income_dict.values()))
+        figc.update_layout(xaxis_title="Diabetes status", 
+                           showlegend = False,
+                            xaxis=dict(tickfont=dict(size=16)),  # X-axis tick font size
+                            yaxis=dict(tickfont=dict(size=16)),  # Y-axis tick font size
+                            xaxis_title_font_size=20,  # X-axis title font size
+                            yaxis_title_font_size=20,  # Y-axis title font size)
+        )
+        st.plotly_chart(figc,use_container_width=True)  
+
+if __name__ == '__main__':
+    main()
---
+++ deploy/pages/Predictor de diabetes.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import joblib
+import numpy as np
+import pandas as pd
+
+#load trained model
+model = joblib.load("trained_model.pkl")
+
+### Define dictionaries: they will be useful later (here in spanish)
+binary_dict = {0:'No',1:'Si'} ### Dictionary for binary variables
+diagnose_dict = {0:'Non-diabetic',1:'Pre-diabetic/Diabetic'} ### Dictionary for the diabetes_status
+sex_dict = {0:'Female',1:'Male' }### Sex Dictionary
+general_health_dict = {1:'Excelente',2:'Muy buena',3:'Buena',4:'Regular',5:'Pobre'} ### General Health Dictionary
+age_dict = {1:'18-24', 2:'25-29', 3:'30-34', 4:'35-39', 5:'40-44', 6:'45-49', 7:'50-54', 8:'55-59', 9:'60-64', 10:'65-69', 11:'70-74', 12:'75-79', 13:'above 80'} ### Age dictionary
+#education_dict = {1:"Kindergarten",2:"Elementary",3:"Some high school",4:"High school graduate",5:"Some college/tecnical school",6:"College graduate"} ### Education
+income_dict = {1:"0-10K",2:"10-15K",3:"15-20K",4:"20-25K",5:"25-30K",6:"30-35K",7:"35-50K",8:"50-75K"}
+
+st.title("Predictor de diabetes")
+
+st.markdown("## Ingrese los datos del paciente para determinar si es diabetico o no:")
+
+#input features
+
+# Input height and weight -> Needed for the BMI
+height = st.number_input("Ingrese la estatura del paciente en metros:", 1.50, 2.00, step=0.01)
+weight = st.number_input("Ingrese el peso del paciente en Kilogramos:", 1, 160, step=1)
+
+#Compute BMI
+BMI = round(weight/(height)**2)
+
+# Input age
+Age = st.radio("Seleccione el grupo de edad del paciente:",
+                  [1,2,3,4,5,6,7,8,9,10,11,12,13],format_func=lambda x: age_dict.get(x),
+                  index=None
+                  )
+#st.write(Age)
+
+# Input age
+Income = st.radio("Seleccione el salario anual del paciente (en dólares):",
+                  [1,2,3,4,5,6,7,8], format_func=lambda x: income_dict.get(x),
+                  index=None
+                  )
+
+# Input your general health
+General_health = st.radio("¿Cómo esta la salud general del paciente?",
+                     [1,2,3,4,5], format_func=lambda x: general_health_dict.get(x),
+                     index=None
+)
+
+# Input the binary features
+
+High_Chol = st.radio("¿tiene el paciente el colesterol alto?",
+                     [0,1], format_func=lambda x: binary_dict.get(x),
+                     index=None
+)
+
+High_BP = st.radio("¿sufre de presión arterial alta el paciente?",
+                     [0,1], format_func=lambda x: binary_dict.get(x),
+                     index=None
+)
+
+Heart_condition = st.radio("¿tiene o ha tenido problemas cardíacos el paciente?",
+                     [0,1], format_func=lambda x: binary_dict.get(x),
+                     index=None
+)
+
+Difficulty_walking = st.radio("¿tiene dificultades para caminar?",
+                     [0,1], format_func=lambda x: binary_dict.get(x),
+                     index=None
+)
+
+Physical_health = st.slider("Durante el último mes, ¿por cuántos días considera que su salud no fue buena?", min_value=0, max_value=30, value=0, step=1)
+
+input_data = np.array([High_BP,High_Chol,BMI,Heart_condition,General_health,Physical_health,Difficulty_walking,Age,Income])
+
+st.markdown("## Diagnostico según el modelo:")
+
+if np.any(input_data == None) == True:
+    st.warning("Debe llenar todos los items")
+else:
+    prediction = model.predict(input_data.reshape(1,-1))
+    predicted_class = diagnose_dict[prediction[0]]
+    #Display the result
+    if prediction == 0:
+        st.success(f"Yor are Non-diabetic")
+    else:
+        st.error(f"You are Diabetic")
+
